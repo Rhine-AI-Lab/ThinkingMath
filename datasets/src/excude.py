f6a9db79cdeede34e15c676e57d358b70f36e586
HRan2004 <2125764918@qq.com>
2023-09-03 20:52:39 +0800

    feat: 新增随机打乱数据

--- datasets/src/excude.py
+++ datasets/src/excude.py
@@ -1,4 +1,7 @@
 import random
+
+from jsonlines import jsonlines
+
 from general_utils import *
 from easy_plus import make_data_plus
 from easy_minus import make_data_minus
@@ -7,15 +10,13 @@ from easy_divide import make_data_divide
 
 tasks = [
     # 数据条数    每个数字的位数-随即范围[min, max]    多少个数字进行相加-随机范围[min>=2, max<=10]
-    ['PLUS', 20, 2, 2, 2, 3],
-    ['PLUS', 10, 2, 5, 3, 8],
-    # ['PLUS', 10, 4, 9, 2, 5],
-    # ['PLUS', 10, 2, 3, 6, 15],
-    ['MINUS', 20, 2, 3],
-    ['MINUS', 10, 3, 8],
-    ['TIMES', 20, 2, 3],
-    ['TIMES', 10, 1, 4],
-    ['DIVIDE', 30, 1, 4],
+    ['PLUS', 1000, 2, 2, 2, 3],
+    ['PLUS', 500, 2, 5, 3, 8],
+    ['MINUS', 1000, 2, 3],
+    ['MINUS', 500, 3, 8],
+    ['TIMES', 1000, 2, 3],
+    ['TIMES', 500, 1, 4],
+    ['DIVIDE', 1500, 1, 4],
 ]
 
 data_sep = '================================'
@@ -57,10 +58,31 @@ for task in tasks:
             nl = [n1, n2]
             print(task[0], nl, '=', f'{n1 // n2}...{n1 % n2}', end='\n\n')
             g_question, g_thinking, g_answer = make_data_divide(nl)
-        data.append([g_question, g_thinking, g_answer])
+        data.append([task[0], g_question, g_thinking, g_answer])
 
 
 file = open('../generate/data_example.txt', 'w', encoding='utf-8')
+jl = jsonlines.open('../generate/think_math_1500x4.jsonl', 'w')
+
+instructions = {
+    'PLUS': '计算一个数学加法运算，列出完整的思考过程，包括每一位的计算，进位，以及思考过程。'
+            '从右边第一位开始，将每个数字的第一位相加，得出的结果，个位作为作为当前位数的运算结果，'
+            '其他的部分作为进位，再前面位数的运算中继续相加，以此类推。'
+            '最后按照从右往左重新排列得出最终运算结果，并再思考部分结束后，列出含有答案的式子给用户。',
+    'MINUS': '计算一个数学减法运算，列出完整的思考过程，包括每一位的计算，进位，以及思考过程。'
+             '从右边第一位开始，对应位置上的数字分别相减，如果不够减，则向前面一位借位，在后续运算前面一位时需额外减一，以此类推。'
+             '最后按照从右往左重新排列得出最终运算结果，并再思考部分结束后，列出含有答案的式子给用户。',
+    'TIMES': '计算一个数学乘法运算，列出完整的思考过程，包括每一位的计算，进位，以及思考过程。'
+             '第一个乘数的每一位与第二个乘数的每一位相乘，第一个乘数得出的结果进行数学加法运算，得出最终答案。'
+             '最后加法部分运算结果按照从右往左重新排列得出最终运算结果，并再思考部分结束后，列出含有答案的式子给用户。',
+    'DIVIDE': '计算一个数学除法运算，列出完整的思考过程，包括每一位的计算，进位，以及思考过程。'
+             '从左往右，每一位都除以除数，余数部分乘以10倍留给下一位。'
+             '最后加法部分运算结果按照从右往左重新排列得出最终运算结果，并再思考部分结束后，列出含有答案的式子给用户。',
+}
+
+random.shuffle(data)
 for line in data:
     file.write(f'{line[0]}\n\n{line[1]}\n\n{line[2]}\n\n{data_sep}\n\n')
+    jl.write({'instruction': instructions[line[0]], 'input': line[1], 'output': line[2] + '\n\n' + line[3]})
 file.close()
+
