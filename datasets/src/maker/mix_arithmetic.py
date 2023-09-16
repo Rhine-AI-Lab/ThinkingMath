7df173fc0ca9078033b4aaaec05eb758d7cf94f9
HRan2004 <2125764918@qq.com>
2023-09-16 14:36:08 +0800

    feat: 支持随机算式

--- datasets/src/maker/mix_arithmetic.py
+++ datasets/src/maker/mix_arithmetic.py
@@ -1,3 +1,4 @@
+import json
 import random
 from general_utils import *
 
@@ -8,73 +9,80 @@ eg = """
 35+10*(15+32)/2-3
 """
 
-ma_len = 8
+ma_len = 6
 operates = ['+', '-', '*', '/']
 
-ras = []
-fhs = []
-kh = []
-
-for i in range(ma_len):
-    ras.append(0)
-    if i != 0:
-        fhs.append(random_str(operates))
-
-for i in range(ma_len - 1):
-    if i not in kh and i + 2 not in kh and i + 1 not in kh:
-        if fhs[i] == '-' and len(fhs) > i + 1 and fhs[i + 1] not in ['*', '/']:
-            kh.append(i + 1)
-        if fhs[i] == '/' and len(fhs) > i + 1:
-            kh.append(i + 1)
-    if i - 1 not in kh and i + 1 not in kh and i not in kh:
-        if fhs[i] in ['+', '-'] and len(fhs) > i + 1 and fhs[i + 1] in ['*', '/']:
-            kh.append(i)
-        if fhs[i] in ['+', '-'] and i > 0 and fhs[i - 1] in ['*', '/']:
-            kh.append(i)
-
-print(kh)
+example = {
+    "left": {
+        "left": 9,
+        "operator": "*",
+        "right": {
+            "left": 10,
+            "operator": "-",
+            "right": 4
+        }
+    },
+    "operator": "+",
+    "right": 7
+}
 
 
-print('先计算括号内内容')
-khr = {}
-pm_ws = [1, 3]
-td_ws = [1, 3]
-for i in kh:
-    print(fhs[i])
-    if fhs[i] == '+':
-        ras[i] = random_len_int(random.randint(pm_ws[0], pm_ws[1]))
-        ras[i+1] = random_len_int(random.randint(pm_ws[0], pm_ws[1]))
-    if fhs[i] == '-':
-        ras[i] = random_len_int(random.randint(pm_ws[0], pm_ws[1]))
-        ras[i+1] = random.randint(10 ** (pm_ws[0] - 1), ras[i])
-    if fhs[i] == '*':
-        ras[i] = random_len_int(random.randint(td_ws[0], td_ws[1]))
-        ras[i+1] = random_len_int(random.randint(td_ws[0], td_ws[1]))
-    if fhs[i] == '/':
-        ras[i+1] = random_len_int(random.randint(td_ws[0], td_ws[1]))
-        ras[i] = ras[i+1] * random_len_int(random.randint(td_ws[0], td_ws[1]))
-
-expression = str(ras[0])
-if 0 in kh:
-    expression = '( ' + expression
-for i in range(len(fhs)):
-    if i - 1 in kh:
-        expression += ' )'
-    expression += f' {fhs[i]}'
-    if i + 1 in kh:
-        expression += ' ('
-    expression += f' {ras[i + 1]}'
-if len(fhs) - 1 in kh:
-    expression += ' )'
-print(expression)
+def is_td(text):
+    return text == '*' or text == '/'
 
 
-try:
-    print('=', eval(expression))
-except:
-    print('= NaN')
-    pass
+def is_pm(text):
+    return text == '+' or text == '-'
 
 
+def is_md(text):
+    return text == '-' or text == '/'
 
+
+# 自定义数值随机生成逻辑
+# operator: 运算符  可用于参考生成
+def random_number(operator):
+    if is_pm(operator):
+        return random.randint(10, 10000)
+    else:
+        return random.randint(10, 100)
+
+
+def random_layer(target, now_len, level):
+    operator = random_str(operates)
+    if now_len == 1:
+        return random_number(operator)
+
+    left_len = random.randint(1, now_len - 1)
+    right_len = now_len - left_len
+
+    target['level'] = level
+    target['left'] = random_layer({}, left_len, level + 1)
+    target['operator'] = operator
+    target['right'] = random_layer({}, right_len, level + 1)
+
+    left_result = target['left'] if left_len == 1 else target['left']['result']
+    right_result = target['right'] if right_len == 1 else target['right']['result']
+    target['result'] = eval(f'{left_result}{target["operator"]}{right_result}')
+
+    left_expression = target['left'] if left_len == 1 else target['left']['expression']
+    right_expression = target['right'] if right_len == 1 else target['right']['expression']
+
+    if is_td(operator):
+        if left_len > 1 and is_pm(target['left']['operator']):
+            left_expression = f'({left_expression})'
+        if right_len > 1 and is_pm(target['right']['operator']):
+            right_expression = f'({right_expression})'
+    if right_len > 1 and is_pm(target['right']['operator']):
+        if is_md(operator) and right_expression[0] != '(':
+            right_expression = f'({right_expression})'
+
+    target['expression'] = f'{left_expression} {target["operator"]} {right_expression}'
+
+    return target
+
+
+expression = random_layer({}, ma_len, 0)
+print(json.dumps(expression, indent=4))
+print(f'\n{expression["expression"]} = {expression["result"]}\n')
 
